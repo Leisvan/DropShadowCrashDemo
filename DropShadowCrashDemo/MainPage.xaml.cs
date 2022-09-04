e652e947939798bc35e5ef54653f4db12d9aa515
Leisvan <rathma@live.com>
2022-09-04 16:02:28 -0500

    Add project files.

---
+++ DropShadowCrashDemo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DropShadowCrashDemo.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DropShadowCrashDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<ItemViewModel> Collection { get; private set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            FillRandomColors();
+        }
+
+        private void FillRandomColors()
+        {
+            var list = new List<ItemViewModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new ItemViewModel());
+            }
+
+            Collection = new ObservableCollection<ItemViewModel>(list);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Remove item at 0 and insert a new item
+            Collection.RemoveAt(0);
+            Collection.Insert(0, new ItemViewModel());
+        }
+    }
+}
---
+++ DropShadowCrashDemo/Models/ItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace DropShadowCrashDemo.Models
+{
+    public class ItemViewModel
+    {
+        private static Random random = new Random();
+        public Brush ColorBrush { get; set; }
+        public ItemViewModel()
+        {
+            ColorBrush = new SolidColorBrush(new Color
+            {
+                R = (byte)random.Next(byte.MaxValue),
+                G = (byte)random.Next(byte.MaxValue),
+                B = (byte)random.Next(byte.MaxValue),
+                A = byte.MaxValue,
+            });
+        }
+    }
+}
